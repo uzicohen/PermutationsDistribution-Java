28f0c0c882df20ce7668be57fe15d2694cee3827
Uzi Cohen <uzicohen9@gmail.com>
2017-10-30 02:57:01 +0200

    First result of the experiment

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+echo "Running script"
+
+mvn clean compile assembly:single
+
+t=$(date +%Y%m%d%H%M%S)
+logName="ex$t.log"
+echo &logName
+
+nohup java -jar /home/uzicohen/Desktop/workspace/PermutationsDistribution/target/PermutationsDistribution-0.0.1-SNAPSHOT-jar-with-dependencies.jar > /home/uzicohen/Desktop/workspace/PermutationsDistribution/src/main/java/resources/experiments/logs/$logName &
+
+while true;
+do
+ tail 10 /home/uzicohen/Desktop/workspace/PermutationsDistribution/src/main/java/resources/experiments/logs/$logName
+ sleep  10 ;
+done
+
+
---
+++ trace.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+while true;
+do
+ echo "============================================================================"
+cat /home/uzicohen/Desktop/workspace/PermutationsDistribution/src/main/java/resources/experiments/output/TM/q0.csv
+echo "============================================================================"
+cat /home/uzicohen/Desktop/workspace/PermutationsDistribution/src/main/java/resources/experiments/output/TM/q1.csv
+echo "============================================================================"
+cat /home/uzicohen/Desktop/workspace/PermutationsDistribution/src/main/java/resources/experiments/output/TM/q2.csv
+echo "============================================================================"
+cat /home/uzicohen/Desktop/workspace/PermutationsDistribution/src/main/java/resources/experiments/output/TM/q3.csv
+ sleep  20 ;
+done
+
+
